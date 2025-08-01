995787d84235e51b549c0963e370bedb95f24a8e
Arjun Mazumder <arjumazumder2@gmail.com>
2025-08-01 08:55:05 +0600

    animal function132 file added

---
+++ animal_function132.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  7 22:39:53 2023
+
+@author: user
+"""
+
+def describe_pet(animal_type, name_pet):
+    print(f"I have a {animal_type.title()}. ")
+    print(f"My {animal_type.title()} name is {name_pet.title()}.")
+describe_pet('hamster', 'snajida')
+describe_pet('dog', 'willie')
+def couple_function(bf_name, gf_name):
+    print(f"I am {bf_name.title()}. I have a girl friend.")
+    print(f"my girl friend name is {gf_name.title()}.")
+couple_function('shahin', 'rupa')   
+
+ 
