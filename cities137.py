698df2fd09696e0db72b3ef614c782a7c3c8a9f2
Arjun Mazumder <arjumazumder2@gmail.com>
2025-08-02 23:39:56 +0600

    cities137 file added

---
+++ cities137.py
@@ -0,0 +1,6 @@
+def describe_city(citys_name, country_name = 'somalia' ):
+    print(f" The city of {citys_name} stay in somalia.")
+describe_city(citys_name = 'merca')
+describe_city(citys_name = 'mogadishu')
+describe_city(citys_name = 'ulaanbaatar')
+describe_city(citys_name = 'astana')    
