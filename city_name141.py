5469d13fcb053b446fbcc4c82dee86728dec13df
Arjun Mazumder <arjumazumder2@gmail.com>
2025-08-03 08:34:08 +0600

    city name141 file added

---
+++ city_name141.py
@@ -0,0 +1,13 @@
+def city_country(city_name, country_name ):
+    information = f"{city_name} {country_name}"
+    return information.title()
+musician_1 = city_country('dhaka', 'bangladesh')
+
+print(musician_1)
+musician_2 = city_country('new delhi','india')
+print(musician_2)
+musician_3 = city_country('washington','america')
+print(musician_3)
+
+
+    
