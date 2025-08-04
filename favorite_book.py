196e0451fb8a5fe6029731c0ecf0d0590fa9f78f
Arjun Mazumder <arjumazumder2@gmail.com>
2025-08-04 10:56:59 +0600

    favorite book problem file added

---
+++ favorite_book.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  7 22:10:46 2023
+
+@author: user
+"""
+
+def favorite_book(title):
+    print(f"My favorite book is {title.title()}!!")
+    
+favorite_book('Python crash course.')    
