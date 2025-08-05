5f018c6b4ffc106adff10351740e3e1f920127cf
Arjun Mazumder <arjumazumder2@gmail.com>
2025-08-05 20:35:05 +0600

    message131 file added

---
+++ message131.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  7 22:05:19 2023
+
+@author: user
+"""
+
+def display_message():
+    print("everyone what you are learning about in this chapter. ")
+    
+display_message()    
