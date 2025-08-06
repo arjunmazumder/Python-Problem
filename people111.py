827f498f8672e737e1f3cff634ce26e1f11bc9e0
Arjun Mazumder <arjumazumder2@gmail.com>
2025-08-06 21:10:45 +0600

    people file added

---
+++ people111.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 24 23:09:56 2023
+
+@author: user
+"""
+
+person_1 = {
+    'first_name' : 'arjun',
+    'last_name' : 'mazunder',
+    'age' : 23,
+    'city' : 'dhaka',
+    }
+person_2 = {
+    'first_name' : 'arina',
+    'last_name' : 'cuty',
+    'age' : 19,
+    'city' : 'london',
+    }
+person_3 = {
+    'first_name' : 'nusrat',
+    'last_name' : 'jahan',
+    'age' : 21,
+    'city' : 'barammanbari',
+    }
+people = [person_1,person_2,person_3]
+for people in people:
+    print(people)
+
+
+
+    
